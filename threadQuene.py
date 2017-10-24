9fae74beeab2de97575b7c3ba881196796868c87
bo.zhang <bo.zhang@etu.unice.fr>
2017-10-24 22:51:48 +0200

    Finish all the tasks except how to move people

--- threadQuene.py
+++ threadQuene.py
@@ -1,27 +1,77 @@
-#coding=utf-8
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import threading
-from time import ctime,sleep
+import random
 
-def music(func):
-    for i in range(2):
-        print ("I was listening to %s. %s" %(func,ctime()))
-        sleep(4)
+local = threading.local()
+lock = threading.Lock()
+terrain = {(0, 0): 0}
+crowd = []
 
-def move(func):
-    for i in range(2):
-        print ("I was at the %s! %s" %(func,ctime()))
-        sleep(5)
 
-threads = []
-t1 = threading.Thread(target=music,args=(u'爱情买卖',))
-threads.append(t1)
-t2 = threading.Thread(target=move,args=(u'阿凡达',))
-threads.append(t2)
+def generateTerrain():
+    a = 0
+    while (a < 512):
+        b = 0
+        while (b < 127):
+            terrain[(a, b)] = 0
+            b = b + 1
+        a = a + 1
+
+
+def generateObstacle():
+    a = 0
+    b = 0
+
+    while (a < 1000):
+        terrain[(random.randint(1, 510), random.randint(1, 125))] = 2
+        a = a + 1
+
+    while (b < 2):
+        c = 0
+        while (c < 2):
+            terrain[(b, c)] = 3
+            c = c + 1
+        b = b + 1
+
+
+def generateCrowd():
+    a = 0
+    while (a < 513):
+        b = random.randint(0, 511)
+        c = random.randint(0, 126)
+        if terrain[(b, c)] == 0:
+            terrain[(b, c)] = 1
+            crowd.append([b, c])
+            a += 1
+
+
+def moveCrowd(*n):
+    lock.acquire()
+    try:
+        print("ffff")
+    finally:
+        lock.release()
+
+
+def movePeople():
+    print("aaaaa")
+
+
+def hasPeople():
+    for (d, x) in terrain.items():
+        if x == 1:
+            return True
+    return False
+
 
 if __name__ == '__main__':
-    for t in threads:
-        t.setDaemon(True)
-        t.start()
-    t.join()
-
-    print ("all over %s" %ctime())
+    generateTerrain()
+    generateObstacle()
+    generateCrowd()
+    while (hasPeople()):
+        for a in crowd:
+            t = threading.Thread(target=moveCrowd, args=(a))
+            t.start()
+            t.join()
